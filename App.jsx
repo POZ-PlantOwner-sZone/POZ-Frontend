a0d4463c3c1e35b2f11a6d16511468cf51cb0471
ajeong <ajung7038@naver.com>
2024-08-25 00:06:55 +0900

    Feat: 식물 등록 화면 추가 #3

--- App.jsx
+++ App.jsx
@@ -1,13 +1,25 @@
 import * as React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
+import { NavigationContainer } from '@react-navigation/native';
+// import { createStackNavigator } from "@react-navigation/stack";
 import LoginScreen from './src/screen/LoginScreen';
+import RegisterPlantScreen from './src/screen/RegisterPlantScreen';
+// import AppNavigator from './AppNavigator';
 
 const App = () => {
+
+  // const Stack = createStackNavigator();
+
   return (
     <NavigationContainer>
-      <LoginScreen />
+      {/* <LoginScreen /> */}
+      <RegisterPlantScreen />
+      {/* <Stack.Navigator initialRouteName="Start"> */}
+        {/* <Stack.Screen name="LoginScreen" component={LoginScreen} /> */}
+        {/* <Stack.Screen name="Main" component={Main} /> */}
+      {/* </Stack.Navigator> */}
     </NavigationContainer>
   );
 };
 
 export default App;
+// export default createAppContainer(AppNavigator);
---
+++ AppNavigator.jsx
@@ -0,0 +1,15 @@
+// import { createStackNavigator } from 'react-navigation-stack';
+// import LoginScreen from './src/screen/LoginScreen';
+// import RegisterPlantScreen from './src/screen/RegisterPlantScreen';
+
+// const AppNavigator = createStackNavigator(
+//     {
+//       Home: LoginScreen,
+//       Details: RegisterPlantScreen,
+//     },
+//     {
+//       initialRouteName: 'Home',
+//     }
+//   );
+
+// export default AppNavigator;
